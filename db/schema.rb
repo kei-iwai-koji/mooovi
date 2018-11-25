d29d4bcbce195320ae73e1ac42974bcc054e2b6e
kei-iwai-koji <iw.kj.16@gmail.com>
2018-11-25 19:12:32 +0900

    Edit rooting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_11_070150) do
+ActiveRecord::Schema.define(version: 2018_11_25_095530) do
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2018_11_11_070150) do
     t.string "director"
     t.text "detail"
     t.string "open_date"
+  end
+
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname"
+    t.integer "rate"
+    t.text "text"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
